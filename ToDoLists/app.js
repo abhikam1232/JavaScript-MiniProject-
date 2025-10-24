16f86012fe94d24ccb2136e0c8a4c60a37a2a296
Abhishek <abhikam152002@gmail.com>
2025-10-24 19:18:48 +0530

    del button implementing

--- ToDoLists/app.js
+++ ToDoLists/app.js
@@ -6,6 +6,19 @@ btn.addEventListener("click", function() {
     let item = document.createElement("li");
     item.innerText = input.value;
     ul.appendChild(item);
-    console.info(input.value);
     input.value = "";
+
+
+    // This part is for creating a del button..
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+    ul.appendChild(delBtn);
+    delBtn.style.marginLeft = "50px";
+    delBtn.style.fontSize = "1.2em";
+
+    delBtn.classList.add("del");
+
 });
+
+
+
