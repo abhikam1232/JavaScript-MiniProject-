27e40ebced1180baf200c4f52c91c96a4db9d986
Abhishek <abhikam152002@gmail.com>
2025-11-22 02:39:53 +0530

    smooth scroll

--- Quiz_App/quiz.js
+++ Quiz_App/quiz.js
@@ -1,5 +1,7 @@
 let startBtn = document.querySelector(".startBtn");
+let secondSection = document.querySelector("#second");
 
 startBtn.addEventListener("click", function() {
-    
-})
+    secondSection.scrollIntoView({ behavior: "smooth"});
+    secondSection.style.transition = "transform 1sec";
+});
