d245e9a2ca36e507d021f945fc097a7e4db4f9f9
Abhishek <abhikam152002@gmail.com>
2025-11-27 21:34:20 +0530

    completed

--- Quiz_App/quiz.js
+++ Quiz_App/quiz.js
@@ -4,9 +4,11 @@ let output = document.querySelector(".output");
 let clickBtn = document.querySelector("#new"); // This is the click button... 
 let h3 = document.querySelector("h3");
 let input = document.querySelector(".answer");
+let submitBtn = document.querySelector("#submit");
+let paragraph = document.querySelector(".newPar");
 
-startBtn.addEventListener("click", function() {
-    secondSection.scrollIntoView({ behavior: "smooth"});
+startBtn.addEventListener("click", function () {
+    secondSection.scrollIntoView({ behavior: "smooth" });
     secondSection.style.transition = "transform 1sec";
 });
 
@@ -21,6 +23,9 @@ const simpleQues = [
 const simpleAns = [
     "blue", "seven", "air", "cold", "earth",
 ];
+
+
+
 
 // Now I basically Want to make an function using which it will automatically display one question and in the input we need to type that answer if that answer is right it will check from the answers array and say it is right... 
 
@@ -37,19 +42,55 @@ par.style.marginTop = "30px";
 
 
 // This is my method which is implemented :- 
-  let index = 0;
+let index = 0;
 
-  clickBtn.addEventListener("click", function() {
-    if ( index < simpleQues.length){
+clickBtn.addEventListener("click", function () {
+    if (index < simpleQues.length) {
         par.innerText = simpleQues[index];
         index = index + 1;
-    } else{
+    } else {
         par.innerText = " ✨ No more Questions are here ✨"
     }
 
-    input.style.visibility = "visible";
+    input.style.visibility = "visible"; // styling for input 
     input.style.width = "200px";
     input.style.margin = "auto";
+    input.style.height = "25px";
+    input.style.padding = "10px";
+
+});
 
 
-  });
+let score = 1;
+
+
+submitBtn.addEventListener("click", function () {
+    let myAnswer = input.value;
+    input.value = "";
+
+    if (simpleAns.includes(myAnswer)) {
+        paragraph.innerText = (`You Scored ${score} point`);
+        score = score + 1;
+    } else {
+        paragraph.innerText = "You are Wrong";
+    }
+
+    paragraph.style.margin = "auto";
+
+
+    // This is to go one the next ques simultaneously..
+
+    
+        if (index < simpleQues.length) {
+            par.innerText = simpleQues[index];
+            index = index + 1;
+        } else {
+            par.innerText = " ✨ No more Questions are here ✨"
+        }
+    
+
+
+
+});
+
+
